85d4b0058ce577cff085e498b2de3629735f0d6e
Yahya-Khwaja <yahyakhwaja222004@gmail.com>
2026-09-09 02:56:30 +0530

    Added model training experiment

---
+++ test_experiment.py
@@ -0,0 +1,53 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from model.model import FedMedModel, train, test
+
+
+transform = transforms.ToTensor()
+
+train_data = datasets.MNIST(
+    root="./data",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+test_data = datasets.MNIST(
+    root="./data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+trainloader = DataLoader(
+    train_data,
+    batch_size=32,
+    shuffle=True
+)
+
+testloader = DataLoader(
+    test_data,
+    batch_size=32
+)
+
+
+for epochs in [1, 2]:
+
+    print(f"\nTraining with {epochs} epoch(s)")
+
+    model = FedMedModel()
+
+    train(
+        model,
+        trainloader,
+        epochs=epochs
+    )
+
+    test_loss, accuracy = test(
+        model,
+        testloader
+    )
+
+    print(f"Test Loss: {test_loss:.4f}")
+    print(f"Test Accuracy: {accuracy:.2f}%")
