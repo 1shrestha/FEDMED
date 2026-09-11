e3874c44b56d5987f495df960aece6e087a2d5c2
Shrestha pandey <pandeyshree141@gmail.com>
2026-09-11 23:32:35 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,18 +1,1 @@
-# model package
-from .simple_model import (
-    MedCNN,
-    MedMLP,
-    flatten_parameters,
-    get_parameters,
-    set_parameters,
-    unflatten_parameters,
-)
-
-__all__ = [
-    "MedMLP",
-    "MedCNN",
-    "get_parameters",
-    "set_parameters",
-    "flatten_parameters",
-    "unflatten_parameters",
-]
+# Proto package
